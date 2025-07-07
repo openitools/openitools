757dc58d629854754e6285496834ba36ea61f6f7
abdullah-albanna <abdu.albanna@proton.me>
2025-07-07 11:10:42 +0300

    build time libs check

--- backend/build.rs
+++ backend/build.rs
@@ -1,3 +1,17 @@
 fn main() {
+    let libs = [
+        "openssl",
+        "gtk+-3.0",
+        "webkit2gtk-4.1",
+        "libimobiledevice-1.0",
+        "libimobiledevice-glue-1.0",
+        "libusbmuxd-2.0",
+    ];
+
+    for lib in libs {
+        if let Err(e) = pkg_config::probe_library(lib) {
+            panic!("Missing system library: `{lib}` not found. Error: {e}");
+        }
+    }
     tauri_build::build();
 }
