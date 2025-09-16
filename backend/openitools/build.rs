dea98552f835817c090e1319e7b5c49a19209355
Abdullah Al-Banna <abdu.albanna@proton.me>
2025-09-17 01:05:21 +0300

    remove gtk requirements in the build.rs

--- backend/openitools/build.rs
+++ backend/openitools/build.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let libs = ["gtk+-3.0", "webkit2gtk-4.1"];
-
-    for lib in libs {
-        if let Err(e) = pkg_config::probe_library(lib) {
-            panic!("Missing system library: `{lib}` not found. Error: {e}");
-        }
-    }
+    // let libs = ["gtk+-3.0", "webkit2gtk-4.1"];
+    //
+    // for lib in libs {
+    //     if let Err(e) = pkg_config::probe_library(lib) {
+    //         panic!("Missing system library: `{lib}` not found. Error: {e}");
+    //     }
+    // }
     tauri_build::build();
 }
